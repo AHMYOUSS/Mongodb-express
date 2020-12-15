ceda3ca1e08fcf4c7f87d422c2ed1126d5ff640a
Ahmed Ismail <41244244+AHMYOUSS@users.noreply.github.com>
2020-12-15 20:19:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('view engine', 'ejs')
+app.set('views', 'views') //default
+
+app.get('/', (req,res,next)=>{
+    res.render('index')
+})
+
+app.listen(3000, (err) => {
+    console.log('App listening on port 3000!');
+});
